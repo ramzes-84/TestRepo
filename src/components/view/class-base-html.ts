2452af6a682819152c51b95485ea61afa8e607a2
Roman Shevyakov <roman@mgstr.ru>
2023-06-22 19:24:45 +0600

    refactor: clean code changes

--- src/components/view/class-base-html.ts
+++ src/components/view/class-base-html.ts
@@ -1,11 +1,11 @@
-export class BaseHTML {
-  public createPageStructure(): void {
-    const body: HTMLBodyElement | null = document.querySelector('body');
-    const answerSect = document.createElement('section');
-    const visualSect = document.createElement('section');
-    const hintSect = document.createElement('section');
-    const codeSect = document.createElement('section');
+export default class BaseHTML {
+    public createPageStructure(): void {
+        const body: HTMLBodyElement | null = document.querySelector('body');
+        const answerSect = document.createElement('section');
+        const visualSect = document.createElement('section');
+        const hintSect = document.createElement('section');
+        const codeSect = document.createElement('section');
 
-    if (body) body.append(answerSect, visualSect, hintSect, codeSect);
-  }
+        if (body) body.append(answerSect, visualSect, hintSect, codeSect);
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './index.html';
 import './index.css';
 import './normalize.css';
-import { BaseHTML } from './components/view/class-base-html';
+import BaseHTML from './components/view/class-base-html';
 // import code from './img/code.png';
 
 const baseHTML = new BaseHTML();

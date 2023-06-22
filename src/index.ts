46dd0883f150ae2ce2faf1c25e014c20c94c78bc
Roman Shevyakov <roman@mgstr.ru>
2023-06-22 10:36:54 +0600

    init: start selectors game task

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,3 @@ import './index.html';
 import './index.css';
 import './normalize.css';
 // import code from './img/code.png';
-
-
-const mult = (a: number, b: number): number => a * b;
-
-console.log(mult(2, 4));
-console.log(mult(3, 4));
-
-const img = new Image();
-// img.src = code;
-
-const imgWrap = document.querySelector('.img');
-if (imgWrap) imgWrap.append(img);
